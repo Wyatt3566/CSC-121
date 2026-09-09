310a29b897239e22305eeb498292a1352f751baf
Wyatt3566 <wwilliams285@access.alamancecc.edu>
2026-09-09 17:06:22 -0400

    Fixed problems

--- assignment/book_tracker.py
+++ assignment/book_tracker.py
@@ -5,27 +5,30 @@ def dashboard():
       📚  YOUR LIBRARY
     ========================================
     """
-    print(f"======================================== \n  📚  YOUR LIBRARY \n========================================")
+
+    print("=" * 40)
+    print("  📚  YOUR LIBRARY ")
+    print("=" * 40)
 
 
 def estimate_reading_time(pages):
  
     #Estemates hours assuming 40 pages/hour rounded to 1 decimal place
-    return(f"{round(int(pages)/40, 1)} hours")
+    return(round(int(pages)/40, 1))
 
 
 def add_book():
 
     #gets input for title, auther, and pages    
-    title = input("Book title: ")
-    author = input("Author: ")
+    title = input("Book title: ").strip().title()
+    author = input("Author: ").strip().title()
     pages = input("Page count: ")
 
     #calls estimate_reading_time by passing in pages
     hours = estimate_reading_time(pages)
 
     #prints book's description
-    print(f"'{title}' by {author} -- approx. {hours} to read")
+    print(f"'{title}' by {author} -- approx. {hours} hours to read")
 
 
 def main():
